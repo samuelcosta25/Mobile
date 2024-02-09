9061969724186dd08b392b1d06be4e02bb0f8513
samuelcosta25 <samu.live@outlook.com>
2024-02-08 21:48:32 -0300

    08.02.23

---
+++ exemplo_variavies_operadores/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  //DECLARANDO E INICIALIZANDO VARIAVEIS
+  String nome = "João";
+  int idade = 25;
+  double altura = 1.75;
+  bool estudante = true;
+
+  // Imprimando variaveis
+  print("Nome: + $nome");
+  print("Idade: + $idade anos");
+  print("Altura: + $altura metros");
+  print("Estudante: + $estudante");
+}
