0638f0e5cf49ebab74b07886df51428fe1320ab6
samuelcosta25 <samu.live@outlook.com>
2024-05-17 17:10:08 -0300

    17.05.24

---
+++ projeto_api_clima_localizacao/lib/Controller/weather_controller.dart
@@ -0,0 +1,20 @@
+import '../Model/weather_model.dart';
+import '../Service/weather_service.dart';
+
+class WeatherController {
+  List<Weather> listWeather = [];
+  WeatherService _service = new WeatherService();
+
+  //métodos
+  Future<void> getFromWeatherService(String city) async{
+    Weather weather = Weather.fromJson(await _service.getWeather(city));
+    listWeather.add(weather);
+  }
+
+  //métodos
+  Future<void> getFromWeatherServiceLocation(double lat, double lon) async{
+    Weather weather = Weather.fromJson(await _service.getWeatherByLocation(lat, lon));
+    listWeather.add(weather);
+  }
+
+}
---
+++ projeto_api_clima_localizacao/lib/View/citydetail_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CityDetailScreen extends StatefulWidget {
+  final String city;
+ CityDetailScreen({required this.city});
+
+  @override
+  State<CityDetailScreen> createState() => _CityDetailScreenState();
+}
+
+class _CityDetailScreenState extends State<CityDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
